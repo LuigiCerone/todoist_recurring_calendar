9aba4f7b2363378fe987fa699cebb40e261dd900
Luigi Cerone <luigicerone96@gmail.com>
2022-10-11 21:33:56 +0200

    Add inquirer and setup basic methods

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from todoist_api_python.api_async import TodoistAPIAsync
 from dotenv import load_dotenv
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+
 
 import asyncio
 import os
@@ -14,7 +17,43 @@ async def get_tasks_async():
         print(error)
 
 
+async def create_task(content: str):
+    try:
+        task = await api.add_task(
+            content=content,
+        )
+        return task
+    except Exception as error:
+        print(error)
+
+
+async def delete_task(task_to_delete: int):
+    try:
+        is_success = await api.delete_task(task_id=task_to_delete)
+        print(is_success)
+    except Exception as error:
+        print(error)
+
+
 async def main():
+
+    action = inquirer.select(
+        message='How do you want to edit your todoist?',
+        choices=[
+            Choice('create', 'Create new recurrent task'), 
+            Choice('delete', 'Delete a bunch of recurrent stuff')
+            ]
+    ).execute()
+
+    if action == 'create':
+        pass
+    elif action == 'delete':
+        pass
+    else:
+        raise Exception('Unknown option specified!')
+
+
+async def get_all_tasks():
     tasks_dict = await get_tasks_async()
     df = pd.DataFrame.from_dict(tasks_dict)
     print(df.head())
